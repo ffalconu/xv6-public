106f42654ec6f0fefcd08e539aa58f22c4770c75
ffalconu <felipefalconurrutia@gmail.com>
2019-04-01 17:26:35 -0300

    Tarea1 travis release v2

--- syscall.c
+++ syscall.c
@@ -117,7 +117,6 @@ static int (*syscalls[])(void) = {
 [SYS_chdir]   sys_chdir,
 [SYS_dup]     sys_dup,
 [SYS_getpid]  sys_getpid,
-[SYS_getprocs]  sys_getprocs,
 [SYS_sbrk]    sys_sbrk,
 [SYS_sleep]   sys_sleep,
 [SYS_uptime]  sys_uptime,
@@ -128,6 +127,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getprocs]    sys_getprocs,
 };
 
 void
